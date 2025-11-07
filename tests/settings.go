e1e45c745b3c09532c33ed2d8beb74cd567822bc
Aqouet <maximryazanoff@yandex.ru>
2025-11-07 06:44:53 +0300

    Add token to tests settings

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,5 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = false
 var Search = true
-var Token = ``
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZF9oYXNoIjoiODllMDE1MzZhYzIwNzI3OTQwOWQ0ZGUxZTUyNTNlMDFmNGExNzY5ZTY5NmRiMGQ2MDYyY2E5YjhmNTY3NjdjOCIsImV4cCI6MTc2MjUxNTA0NCwiaWF0IjoxNzYyNDg2MjQ0fQ.Apo44nOXegRUznfqpxdkiwU-ayacGABBWCx0qNcVkQw`
+
